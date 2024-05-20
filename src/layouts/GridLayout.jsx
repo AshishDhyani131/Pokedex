ae77a079ae3c25ed1725d00d8b3d07653db738bd
AshishDhyani131 <ashishdhyani100@gmail.com>
2024-05-20 19:06:09 +0530

    minor changes

--- src/layouts/GridLayout.jsx
+++ src/layouts/GridLayout.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const GridLayout = ({ children, ...props }) => {
   return (
     <div
-      className="w-screen h-screen  relative grid grid-rows-rootLayout rounded-md sm:w-[300px] sm:h-[505px]"
+      className="w-screen h-screen  relative grid grid-rows-rootLayout rounded-md sm:w-[300px] sm:h-[580px]"
       {...props}
     >
       {children}
